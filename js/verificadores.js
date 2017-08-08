d5248404c0396b50b8b9d597b0c59a093ddcd9a8
ivanneira <ien1983@gmail.com>
2017-08-08 20:55:26 -0300

    agregado base de datos nedb

--- js/verificadores.js
+++ js/verificadores.js
@@ -1,22 +1,28 @@
 /**
  * Created by Ivan on 07/08/2017.
  */
+//la validación es del tipo "validar a menos que haya un falso"
+//solo se valida que el campo no esté vacío
 
 var validador = true;
 
-var controles = [
-    "#dateCarga",
-    "#inpPNombre",
-    "#datePNacimiento",
-    "#inpPDNI",
-    "#inpPDom"
-];
 
-function verificar() {
+//funcion principal
+function verificar(){
+
+    validador = true;
+
+    //controles a validar
+    var controles = [
+        "#dateCarga",
+        "#inpPNombre",
+        "#datePNacimiento",
+        "#inpPDNI",
+        "#inpPDom"
+    ];
+
 
     for (var i = 0; i < controles.length; i++) {
-
-        validador = true;
 
         if (isEmpty(controles[i])) {
             removeMark(controles[i]);
@@ -26,16 +32,17 @@ function verificar() {
             validador = false;
         }
 
-
     }
 
+    //valida que se seleccione al menos un radio button
     if($('input:radio[name="optradio"]:checked').length<=0){
         mark($("#tipoDeTurno"));
+        validador = false;
     }else{
         removeMark($("#tipoDeTurno"));
     }
 
-
+    //si es programado debe especificar fecha, sino no
     if(($("#selProgramado").val() == 1)&&($("#selAtendido").val() == 0)){
 
 
@@ -47,8 +54,10 @@ function verificar() {
             validador = false;
         }
     }
-}
 
+    return validador;
+}
+//está vacío?
 function isEmpty(selector){
 
     if($(selector).val() == ""){
@@ -60,17 +69,41 @@ function isEmpty(selector){
     }
 }
 
+//agrega clase a campos erróneos
 function mark(selector){
 
     $(selector).addClass("incorrecto");
-    console.log("afsdsdaf")
+
 }
 
-function mark(selector){
-    $(selector).addClass("incorrecto");
-}
-
+//quita la marca de incorrecto
 function removeMark(selector){
 
     $(selector).removeClass("incorrecto");
+}
+
+//reinicia algunos campos para la próxima encuesta
+function removeSavedData(){
+
+    //campos a reestablecer
+    var camposABorrar = [
+
+        "#inpPNombre",
+        "#datePNacimiento",
+        "#inpPDNI",
+        "#inpPDom",
+        "#Edad",
+        "#inpPTel",
+        "#dateTurno"
+    ];
+
+    for(var i = 0 ; i < camposABorrar.length ; i++){
+
+        $(camposABorrar[i]).val("");
+    }
+
+    //deselecciona los radio y quita el html asociado
+    $('input:radio[name="optradio"]:checked').prop('checked',false);
+    $("#contTurno").html("");
+
 }
--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
-
+//*******************************************
+//para hacer un build ejecutable en windows
+//electron-packager "c:\Users\Ivan\Desktop\para protur\proturdapp" proturdapp --win
+//*******************************************
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
